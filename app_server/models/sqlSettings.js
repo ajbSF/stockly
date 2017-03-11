60b184d527e2542e97285ddfb4da92a24d79fe23
AJB <ajbSF@users.noreply.github.com>
2017-03-10 16:35:50 -0800

    Added files to ignore

--- app_server/models/sqlSettings.js
+++
@@ -1,5 +0,0 @@
-module.exports  = {
-                          host     : 'localhost',
-                          user     : 'root',
-                          password : 'pa',
-                          database : 'stockly'};
