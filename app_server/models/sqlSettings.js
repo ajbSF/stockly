aec80a4594464ad8fdb6fb83ce9108228164a1ec
AJB <ajbSF@users.noreply.github.com>
2017-03-10 16:27:09 -0800

    Testing ignore

--- app_server/models/sqlSettings.js
+++ app_server/models/sqlSettings.js
@@ -1,5 +1,5 @@
 module.exports  = {
                           host     : 'localhost',
                           user     : 'root',
-                          password : '', /**********************************/
+                          password : 'pa',
                           database : 'stockly'};
